2f17605dc0383b29f596cdcd6fe30e17074f732b
Sheri Richardson <shrdson@gmail.com>
2017-11-09 18:45:25 +0100

    Add animated reveal of menu

--- 007-notifications/js/script.js
+++ 007-notifications/js/script.js
@@ -1,8 +1,16 @@
 $(document).ready(function() {
   const search = document.querySelector(".search-icon");
   const input = document.querySelector(".search-field");
+  const menuIcon = document.querySelector(".menu-icon");
+  const menu = document.querySelector(".menu");
+  const panel = document.querySelector(".notif-panel");
 
   $(search).click(function() {
     $(input).toggleClass("active");
+  });
+
+  $(menuIcon).click(function() {
+    $(panel).toggleClass("reveal-menu");
+    $(menu).toggleClass("reveal-menu");
   });
 });
